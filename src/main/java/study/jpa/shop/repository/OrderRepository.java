9a82e520ce8e44f178d99bfc440f65ddb61fc311
pch8388 <pch838811@gmail.com>
2020-01-17 23:56:02 +0900

    [feat] : 주문 repository  추가

---
+++ src/main/java/study/jpa/shop/repository/OrderRepository.java
@@ -0,0 +1,27 @@
+package study.jpa.shop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.jpa.shop.domain.Order;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
+}
