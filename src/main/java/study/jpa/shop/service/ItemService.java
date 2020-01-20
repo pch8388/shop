2873206065ba39c8c4a64613a5923985e2a72690
pch8388 <pch838811@gmail.com>
2020-01-20 23:39:51 +0900

    [feat] : 상품등록 & 목록 view

--- src/main/java/study/jpa/shop/service/ItemService.java
+++ src/main/java/study/jpa/shop/service/ItemService.java
@@ -2,6 +2,7 @@ package study.jpa.shop.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import study.jpa.shop.domain.item.Item;
 import study.jpa.shop.repository.ItemRepository;
 
@@ -9,10 +10,12 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
