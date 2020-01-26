fb9435422303726ed30921a8c390e6d112060e6a
pch8388 <pch838811@gmail.com>
2020-01-26 22:43:12 +0900

    [feat] : member 수정 api

--- src/main/java/study/jpa/shop/api/MemberApiController.java
+++ src/main/java/study/jpa/shop/api/MemberApiController.java
@@ -1,10 +1,9 @@
 package study.jpa.shop.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.jpa.shop.domain.Member;
 import study.jpa.shop.service.MemberService;
 
@@ -30,6 +29,30 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+        @PathVariable("id") Long id,
+        @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
     }
 
     @Data
