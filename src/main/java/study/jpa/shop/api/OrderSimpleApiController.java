cac717aa48d5efb1ad2cdb906d5f8f1353335918
pch8388 <pch838811@gmail.com>
2020-01-28 01:21:16 +0900

    [feat] : order 조회 v2 - dto 로 변환 - n + 1 문제 발생

--- src/main/java/study/jpa/shop/api/OrderSimpleApiController.java
+++ src/main/java/study/jpa/shop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package study.jpa.shop.api;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.jpa.shop.domain.Address;
 import study.jpa.shop.domain.Order;
+import study.jpa.shop.domain.OrderStatus;
 import study.jpa.shop.repository.OrderRepository;
 import study.jpa.shop.repository.OrderSearch;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -25,4 +32,30 @@ public class OrderSimpleApiController {
         List<Order> all = orderRepository.findAll(new OrderSearch());
         return all;
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        return orderRepository.findAll(new OrderSearch())
+            .stream()
+            .map(SimpleOrderDto::new)
+            .collect(toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+    }
+
 }
