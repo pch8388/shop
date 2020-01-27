e9d489c73c61d0483f251174a529ffabd354f9df
pch8388 <pch838811@gmail.com>
2020-01-28 01:30:48 +0900

    [feat] : order 조회 v3 - dto 로 변환 - fetch join 사용

--- src/main/java/study/jpa/shop/api/OrderSimpleApiController.java
+++ src/main/java/study/jpa/shop/api/OrderSimpleApiController.java
@@ -41,6 +41,14 @@ public class OrderSimpleApiController {
             .collect(toList());
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        return orderRepository.findAllWithMemberDelivery()
+            .stream()
+            .map(SimpleOrderDto::new)
+            .collect(toList());
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
