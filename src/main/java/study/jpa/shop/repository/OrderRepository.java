12b12f9db9baba4b76cdbf6e071de3144882bf35
pch8388 <pch838811@gmail.com>
2020-02-01 22:42:36 +0900

    [feat] : order 조회 OneToMany v3 - dto 로 변환 - fetch 조인

--- src/main/java/study/jpa/shop/repository/OrderRepository.java
+++ src/main/java/study/jpa/shop/repository/OrderRepository.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -53,6 +54,16 @@ public class OrderRepository {
         "select o from Order o" +
                 " join fetch o.member m" +
                 " join fetch o.delivery d", Order.class)
+            .getResultList();
+    }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+            "select distinct o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d" +
+                " join fetch o.orderItems oi" +
+                " join fetch oi.item i", Order.class)
             .getResultList();
     }
 }
